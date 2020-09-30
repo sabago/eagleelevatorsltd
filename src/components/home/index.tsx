82f9bcd2103887cb9fac6db586b8d87fd5227b6c
sabago <sandra.abago@gmail.com>
2020-09-30 19:28:43 -0400

    temp console log

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -8,7 +8,7 @@ import { Services } from '../services';
 import { Clients } from '../clients';
 import { Contact } from '../contact-us';
 
-
+console.log("home");
   
 export class Home extends React.Component {
     render() {
