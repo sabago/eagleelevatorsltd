fa71d8b525d215f0a179ff797266439dbbace608
sabago <sandra.abago@gmail.com>
2020-10-01 16:43:47 -0400

    fixing express

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const app = express();
 // app.use(express.static(__dirname + '/client/build'))
 
 const path = require('path')
-let root = path.join(__dirname, '..', 'build/')
+let root = path.join(__dirname, 'client', 'build/')
 app.use(express.static(root))
 app.use(function(req, res, next) {
   if (req.method === 'GET' && req.accepts('html') && !req.is('json') && !req.path.includes('.')) {
     res.sendFile('index.html', { root })
   } else next()
 })
-
+// app.use('/relativepath',express.static(path.join(__dirname, 'build')));
 // const root = require('path').join(__dirname, 'client', 'build')
 // app.use(express.static(root));
 // app.get("*", (req, res) => {
