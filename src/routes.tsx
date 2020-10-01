94e27edc0029aedbfdf3d6dded1f8ee6461e718d
sabago <sandra.abago@gmail.com>
2020-10-01 08:28:41 -0400

    switching to hashrouter

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import ScrollToTop from "./scrollTop";
 
 import { Home } from "./components/home";
