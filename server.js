059a02d1fcee31dfa35c86ac80400df2165cf59b
sabago <sandra.abago@gmail.com>
2020-10-01 16:22:48 -0400

    Fixing express server, and basename

--- server.js
+++ server.js
@@ -2,7 +2,21 @@ let express = require('express');
 
 
 const app = express();
-// app.use(cors())
-// app.use(express.json())
-app.use(express.static(__dirname + '/client/build'))
+// // app.use(cors())
+// // app.use(express.json())
+// app.use(express.static(__dirname + '/client/build'))
 
+const path = require('path')
+let root = path.join(__dirname, '..', 'build/')
+app.use(express.static(root))
+app.use(function(req, res, next) {
+  if (req.method === 'GET' && req.accepts('html') && !req.is('json') && !req.path.includes('.')) {
+    res.sendFile('index.html', { root })
+  } else next()
+})
+
+// const root = require('path').join(__dirname, 'client', 'build')
+// app.use(express.static(root));
+// app.get("*", (req, res) => {
+//     res.sendFile('index.html', { root });
+// })
